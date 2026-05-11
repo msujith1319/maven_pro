6dec75905ddfd4ebf89616128429b2b828185512
msujith1319 <msujith1319@gmail.com>
2026-05-11 09:04:07 +0100

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,14 @@
+package com.example;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Maven 🚀");
+        System.out.println("Application started successfully");
+        while (true) {
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
     }
 }
