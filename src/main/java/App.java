704f033ca0e0f57e7a2936f05927ed84a719cce8
msujith1319 <msujith1319@gmail.com>
2026-05-11 09:52:01 +0100

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("hello Sujith");
+        System.out.println("hello");
         while (true) {
             try {
                 Thread.sleep(10000);
